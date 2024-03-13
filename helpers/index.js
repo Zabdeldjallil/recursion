dfad82b55bd98b2675c9085d10b2fd0ef8cadce2
Abderrahman-Kouider <abderrahman.kouider@gmail.com>
2024-03-13 17:48:10 +0100

    added string reversal function and palindrome checker function and imported them in main.js

--- helpers/index.js
+++ helpers/index.js
@@ -7,3 +7,18 @@ export function countDown(n) {
   }
 
 }
+
+export function palindromeChecker(str){
+  if(str.length === 1) return true;
+  if(str.length === 2) return str[0] === str [1];
+  if(str[0] === str.slice(-1)) return palindromeChecker(str.slice(1,-1))
+  return false
+}
+
+export function stringReversal(str) {
+  if (str === "") {
+    return "";
+} else {
+    return stringReversal(str.substr(1)) + str.charAt(0);
+}
+}
--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 import { countDown } from "./helpers/index"
 
 console.log(countDown(3))
+
+import { stringReversal } from "./helpers/index"
+
+console.log(stringReversal('hello world'))
+
+import { palindromeChecker } from "./helpers/index"
+
+console.log(palindromeChecker('tacocat'))
